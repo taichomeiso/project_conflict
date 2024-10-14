dd8f34c0271f1cdaa5ba75d347cbc2b1495d55fd
taichomeiso <otaichididit@gmail.com>
2024-10-14 16:54:16 +0900

    コントローラールーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'results/index'
+  get 'games/index'
+  root to: 'tops#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
